e0b11440899085958d77e2bdb6747b5ecdb5118a
yash <yaxj28@gmail.com>
2026-07-25 20:29:15 +0530

    fix: restore full viewport width (100vw) for horizontal stripe dividers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ const BLUR_FADE_DELAY = 0.04;
 
 export default function Page() {
   const StripeDivider = () => (
-    <div className="relative -mx-4 sm:-mx-6 md:-mx-10 w-[calc(100%+2rem)] sm:w-[calc(100%+3rem)] md:w-[calc(100%+5rem)] h-[30px] bg-diagonal-stripes opacity-40 border-y border-dashed border-black/20 dark:border-white/30 pointer-events-none my-2"></div>
+    <div className="relative w-[100vw] left-1/2 -translate-x-1/2 h-[30px] bg-diagonal-stripes opacity-40 border-y border-dashed border-black/20 dark:border-white/30 pointer-events-none my-2"></div>
   );
 
   return (
